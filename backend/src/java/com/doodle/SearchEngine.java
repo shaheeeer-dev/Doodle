12ba3c44d86aa7e2a17fbb432c78d30d55738ed9
Muhammad Shaheer <shaheeramazon1@gmail.com>
2026-04-24 22:03:23 +0500

    added basic Document.java base class and InvertedIndex.java for keywoord lookup

---
+++ backend/src/java/com/doodle/SearchEngine.java
@@ -0,0 +1,58 @@
+package src.java.com.doodle;
+
+public class SearchEngine {
+}
+
+        int id = nextId++;
+        Document doc = new Document(id, content, title);
+
+        documentStore.put(id, doc);
+
+        String combinedText = content + " " + (title != null ? title : "");
+        invertedIndex.addDocument(id, combinedText);
+    }
+
+    //Search (ranked)
+    public List<Document> search(String query) {
+        Map<Integer, Integer> scores = invertedIndex.searchMultipleWords(query);
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(scores.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        List<Document> results = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> entry : list) {
+            Document doc = documentStore.get(entry.getKey());
+            if (doc != null) {
+                results.add(doc);
+            }
+        }
+
+        return results;
+    }
+
+    public void save(String path) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path))) {
+            out.writeObject(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SearchEngine load(String path) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(path))) {
+            return (SearchEngine) in.readObject();
+        } catch (Exception e) {
+            return new SearchEngine(); // fresh start
+        }
+    }
+
+    public Document getDocument(int id) {
+        return documentStore.get(id);
+    }
+
+    public int getTotalDocuments() {
+        return documentStore.size();
+    }
+}
